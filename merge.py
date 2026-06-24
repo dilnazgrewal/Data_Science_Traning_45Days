ab83ad8f390a317e16617448866117dfe3d78824
dilnazgrewal <dilnazkaur285@gmail.com>
2026-06-24 17:46:38 +0530

    Removed unwanted file from Git tracking

--- merge.py
+++
@@ -1,9 +0,0 @@
-import nbformat
-
-nb1 = nbformat.read("control_flow_statements (1).ipynb", as_version=4)
-nb2 = nbformat.read("python_operators (1).ipynb", as_version=4)
-
-merged = nb1
-merged.cells.extend(nb2.cells)
-
-nbformat.write(merged, "merged.ipynb")
